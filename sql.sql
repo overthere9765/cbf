d40657e6d5465436f05357d435ac67d634fec6e1
Simon <simon72217@gmail.com>
2022-12-10 13:00:51 +0700

    a

--- sql.sql
+++ sql.sql
@@ -14,20 +14,28 @@ VALUES(
     '${spender}',
     $ { note }
 );
-create database wea;
--- permission
-CREATE USER 'muser' @'localhost' IDENTIFIED BY 'uWnfj&20dsl';
-CREATE USER 'muser' @'%' IDENTIFIED BY 'uWnfj&20dsl';
-GRANT ALL PRIVILEGES ON *.* TO 'muser' @'localhost' IDENTIFIED BY 'PASSWORD' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'muser' @'%' IDENTIFIED BY 'PASSWORD' WITH GRANT OPTION;
 
-GRANT ALL ON wea.* TO muser@'localhost' IDENTIFIED BY 'uWnfj&20dsl';
-GRANT ALL ON wea.* TO muser@'%' IDENTIFIED BY 'uWnfj&20dsl';
-GRANT ALL ON wea.* TO muser@'155.138.244.125' IDENTIFIED BY 'uWnfj&20dsl';
+-- reset password root
+use mysql;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+flush privileges;
+quit
+
+mysql -u root -p
+
+-- permission
+CREATE USER 'muser' @'localhost' IDENTIFIED BY 'password';
+CREATE USER 'muser' @'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'muser' @'localhost'  WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'muser' @'%' WITH GRANT OPTION;
+
+mysql -u muser -p wea < wea.sql
+create database wea;
+
 
 -- check users
 select host, user, password, max_connections, max_user_connections, Grant_priv from mysql.user;
--- GRANT ALL PRIVILEGES ON `wea`.* TO 's' @'localhost';
+
 -- change password
 ALTER USER 'muser' @'%' IDENTIFIED BY 'NEW_USER_PASSWORD';
 FLUSH PRIVILEGES;
@@ -35,6 +43,9 @@ FLUSH PRIVILEGES;
 -- import wea
 mysql -u muser -p wea < wea.sql
 
+-- check tables in database
+use wea;
+show tables;
 
 -- clear data
 truncate table approveds;
@@ -42,11 +53,3 @@ truncate table transfereds;
 truncate table transfererrors;
 
 select * from transfererrors;
-
-mysql -u root -p
-
-CREATE USER s IDENTIFIED by 'passwordS@1';
-GRANT ALL ON wea.* TO 's'@'localhost';
-
-FLUSH PRIVILEGES;
-EXIT
